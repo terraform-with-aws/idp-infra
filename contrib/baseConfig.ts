ebb303acd54d09f3768914375b04eaf8cda3491d
Shriyanshu Raj <shriyanshu@arkayalabs.com>
2025-02-21 12:43:37 +0530

    some changes

---
+++ contrib/baseConfig.ts
@@ -0,0 +1,22 @@
+import { SecurityGroup, Subnet } from "@cdktf/provider-aws/lib/vpc";
+import { BaseStack } from "../base";
+
+export interface BaseConfig {
+  vpcId: string;
+  publicSecurityGroup: SecurityGroup;
+  appSecurityGroup: SecurityGroup;
+  publicSubnets: Subnet[];
+  appSubnet: Subnet[];
+  ecsClusterName: string;
+}
+
+export default function getBaseConfig (devBase: BaseStack): BaseConfig {
+  return {
+    vpcId: devBase.vpc.id,
+    publicSecurityGroup: devBase.publicSecurityGroup,
+    publicSubnets: devBase.publicSubnets,
+    ecsClusterName: devBase.ecsCluster.name,
+    appSecurityGroup: devBase.appSecurityGroup,
+    appSubnet: devBase.appSubnets,
+  };
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { App } from "cdktf";
 import { BaseStack } from "./base";
 import PetStack from "./contrib/PetApp";
+import getBaseConfig from "./contrib/baseConfig";
 
 const app = new App();
 //@ts-ignore
@@ -8,22 +9,13 @@ const devBase = new BaseStack(app, "infra", {
   profile: "ark-dev",
 });
 
+
+
 new PetStack(app, "pet-app", {
+  ...getBaseConfig(devBase),
   profile: "ark-dev",
-  vpcId: devBase.vpc.id,
-  publicSecurityGroup: devBase.publicSecurityGroup,
-  publicSubnets: devBase.publicSubnets,
-  ecsClusterName: devBase.ecsCluster.name,
-  appSecurityGroup: devBase.appSecurityGroup,
-  appSubnet: devBase.appSubnets,
   repository: 'terraform-with-aws/apps-petapp',
   branch: 'main'
 });
 
-new pet-app(app, "pet-app", {
-  ...getBaseConfig(devBase),
-  profile: "ark-dev",
-  repository: "terraform-with-aws/apps-petapp",
-  branch: "main"
-})
 app.synth();
