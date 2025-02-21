3a38055f61a0cd14439c8dc5a91c979d985181a4
Shriyanshu Raj <shriyanshu@arkayalabs.com>
2025-02-21 15:38:05 +0530

    delete pet-app-1 environment

--- main.ts
+++ main.ts
@@ -17,11 +17,4 @@ new PetStack(app, "pet-app", {
   owner: "admin",
 });
 
-new PetStack(app, "pet-app-1", {
-  ...getBaseConfig(devBase),
-  owner: "rajshriyanshu5@gmail.com",
-  profile: "ark-dev",
-  repository: "terraform-with-aws/apps-petapp",
-  branch: "main"
-})
 app.synth();
